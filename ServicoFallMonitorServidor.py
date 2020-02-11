6a1a0df731b9c4d4784da1276047a2a95c3146c4
Daniel Veiga <daveigantu@gmail.com>
2020-02-11 15:16:52 -0300

    DataFrame Online e com atualização implementada.

--- ServicoFallMonitorServidor.py
+++ ServicoFallMonitorServidor.py
@@ -9,13 +9,23 @@ from flask import Flask, jsonify, request
 import json
 import os
 
-app = Flask(__name__)
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 MAX_SIZE = 196
-dataframe = pd.read_csv("DataFrame.csv", sep=';')
+app = Flask(__name__)
 
-x = np.array(dataframe.drop('legenda',1))
-y = np.array(dataframe.legenda)
+def pegaDataFrame(key, sheetID, jsonLogin):
+    scope = ['https://spreadsheets.google.com/feeds']
+
+    credentials = ServiceAccountCredentials.from_json_keyfile_dict(jsonLogin, scope)
+    ServiceAccountCredentials
+
+    gc = gspread.authorize(credentials)
+
+    wks = gc.open_by_key(key).get_worksheet(sheetID)
+
+    return wks
 
 def normaliza(dados):
     Tdados = len(dados)
@@ -44,50 +54,90 @@ def queda(_kmeans,GKF_lista):
     
     return v > f
 
-def kmeans(entradas):
+def kmeans(entradas, x):
     kmeans = KMeans(n_clusters=9, random_state=0)
     kmeans.fit(x)
     return int((kmeans.predict(entradas))[0])
 
-def gaussiannb(entradas):
+def gaussiannb(entradas, x, y):
     gaussiannb = GaussianNB() 
     gaussiannb.fit(x, y)
     return (gaussiannb.predict(entradas))[0]
 
-def kNN(entradas):
+def kNN(entradas, x, y):
     kNN = KNeighborsClassifier(n_neighbors=3)
     kNN.fit(x,y)
     return (kNN.predict(entradas))[0]
     
-def florest(entradas):
+def florest(entradas, x, y):
     florest = RandomForestClassifier()
     florest.fit(x, y)
     return (florest.predict(entradas))[0]
 
 @app.route('/atualiza', methods=['POST'])
 def atualiza():
-    return "nao implementado"
+    try:
+        key         = request.get_json().get('key')
+        sheetID     = request.get_json().get('sheetID')
+        jsonLogin   = request.get_json().get('jsonLogin')
+        dados       = normaliza(request.get_json().get('dados'))
+        legenda     = [request.get_json().get('legenda')]
+
+        entrada = legenda + dados
+
+        wks = pegaDataFrame(key,sheetID,jsonLogin)
+        data = wks.get_all_values()
+        headers = data.pop(0)
+        dataframe = pd.DataFrame(data, columns=headers)
+
+        numLinhas = dataframe.legenda.count() + 1
+
+        cell_list = wks.range("A" + str(numLinhas + 1) + ":GO" + str(numLinhas + 1))
+
+        item = 0
+        for cell in cell_list:
+            cell.value = entrada[item]
+            item += 1
+
+        wks.update_cells(cell_list)
+
+        return jsonify({"retorno":"ok"})
+
+    except Exception as e: 
+        return jsonify({"retorno":e})
 
 @app.route('/avaliacao', methods=['POST'])
 def avaliacao():
 
-    dados = request.get_json().get('dados')
-    entrada = np.array([normaliza(dados)])
+    key         = request.get_json().get('key')
+    sheetID     = request.get_json().get('sheetID')
+    jsonLogin   = request.get_json().get('jsonLogin')
+    dados       = request.get_json().get('dados')
+    entrada     = np.array([normaliza(dados)])
 
-    _kmeans = kmeans(entrada)
-    _gaussiannb = gaussiannb(entrada)
-    _kNN = kNN(entrada)
-    _florest = florest(entrada)
+    wks = pegaDataFrame(key,sheetID,jsonLogin)
 
-    _queda = queda(_kmeans,[_gaussiannb,_kNN,_florest])
+    data = wks.get_all_values()
+    headers = data.pop(0)
+
+    dataframe = pd.DataFrame(data, columns=headers)
+
+    x = np.array(dataframe.drop('legenda',1)).astype(np.float)
+    y = np.array(dataframe.legenda)
+
+    _kmeans     = kmeans(entrada, x)
+    _gaussiannb = gaussiannb(entrada, x, y)
+    _kNN        = kNN(entrada, x, y)
+    _florest    = florest(entrada, x, y)
+    _queda      = queda(_kmeans,[_gaussiannb,_kNN,_florest])
 
     return jsonify({
         "resultado":{
-            "kmeans":_kmeans,
-            "gaussiannb":_gaussiannb,
-            "kNN":_kNN,
-            "florest":_florest,
-            "queda":_queda
+            "kmeans":       _kmeans,
+            "gaussiannb":   _gaussiannb,
+            "kNN":          _kNN,
+            "florest":      _florest,
+            "queda":        _queda
          }
     })
 
