c5da13a843f64889ba79abd51c626d5919e53713
Daniel Veiga <daveigantu@gmail.com>
2020-02-11 16:02:01 -0300

    Dev mode desativado

--- ServicoFallMonitorServidor.py
+++ ServicoFallMonitorServidor.py
@@ -143,5 +143,5 @@ def avaliacao():
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    #app.run(debug=False, host='0.0.0.0', port=port)
-    app.run(debug=True, host='127.0.0.1', port=port) 
+    app.run(debug=False, host='0.0.0.0', port=port)
+    #app.run(debug=True, host='127.0.0.1', port=port) 
